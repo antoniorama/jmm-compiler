eefd4d8e7729cb82aadea863ec2b45457ffb4e02
antoniorama <antonio.rama.03@gmail.com>
2024-04-16 19:36:52 +0100

    added test (not passing)

--- test/pt/up/fe/comp/cp2/SemanticAnalysisTest.java
+++ test/pt/up/fe/comp/cp2/SemanticAnalysisTest.java
@@ -224,4 +224,12 @@ public class SemanticAnalysisTest {
         TestUtils.mustFail(result);
         System.out.println(result.getReports());
     }
+
+    @Test
+    public void privMemberAccessWrong() {
+        var result = TestUtils
+                .analyse(SpecsIo.getResource("pt/up/fe/comp/cp2/semanticanalysis/MemberAccessWrong.jmm"));
+        TestUtils.mustFail(result);
+        System.out.println(result.getReports());
+    }
 }
